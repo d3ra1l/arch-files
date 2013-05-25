15c807b0aed3e16abd2e5f9f46f3530de079c831
phallus <cvondoper@gmail.com>
2013-05-25 01:53:58 +0000

    updated removals

--- config/awesome/daze2/layout/init.lua
+++
@@ -1,2 +0,0 @@
-require("daze.layout.tile")
-module("daze.layout")
--- config/awesome/daze2/widgets/init.lua
+++
@@ -1,7 +0,0 @@
-require("daze.widgets.calendar")
-require("daze.widgets.mpd")
-require("daze.widgets.net")
-require("daze.widgets.sys")
-require("daze.widgets.bat")
-module("daze.widgets")
-
