8c26449f8c9f653cb0300c74618bec28fefb624c
phallus <cvondoper@gmail.com>
2013-08-19 23:58:34 +0000

    literal autism

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -59,8 +59,8 @@ modkey = "Mod1"
 -- Table of layouts to cover with awful.layout.inc, order matters.
 local layouts =
 {
-    awful.layout.suit.floating,
-    awful.layout.suit.tile,
+	awful.layout.suit.floating,
+	awful.layout.suit.tile,
 	daze.layout.dtile,
 }
 
