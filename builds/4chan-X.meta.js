a233b7399acc4c5147f5a831b17583a3bbee0388
phallus <cvondoper@gmail.com>
2015-12-13 00:20:42 -0600

    things

---
+++ builds/4chan-X.meta.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name         4chan X
+// @version      3.13.15
+// @minGMVer     1.14
+// @minFFVer     26
+// @namespace    4chan-X
+// @description  Essentially it's 4chan X plus more bloat.
+// @license      MIT; https://github.com/ihavenoface/4chan-x/blob/v3/LICENSE
+// @match        *://boards.4chan.org/*
+// @match        *://sys.4chan.org/*
+// @match        *://a.4cdn.org/*
+// @match        *://i.4cdn.org/*
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @grant        GM_deleteValue
+// @grant        GM_listValues
+// @grant        GM_openInTab
+// @grant        GM_xmlhttpRequest
+// @run-at       document-start
+// @updateURL    https://4chan-x.org/stable-v3/4chan-X.meta.js
+// @downloadURL  https://4chan-x.org/stable-v3/4chan-X.user.js
+// @icon         data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgAgMAAAAOFJJnAAAACVBMVEUAAGcAAABmzDNZt9VtAAAAAXRSTlMAQObYZgAAAE5JREFUeF6NyiEOwEAMA0ET/88kJP8L2VdWKomuR7poJFu/qkiuhcxITRZqYkYLwzRkoQIoLaSG0QFDDtwfw7stmmnmRPxBx3PAlpLF3QN2aj/BUuOfdAAAAABJRU5ErkJggg==
+// ==/UserScript==
